45994a346105f90ed2e1e2a980b885bace4ca4a5
Kafui Alordo <kalordo7@gmail.com>
2024-06-25 18:23:59 +0000

    Implement the code sample for this lesson

---
+++ basic-web-coding/code/script.js
@@ -0,0 +1,4 @@
+function togglePythonInfo() {
+    const pythonBenefits = document.getElementById('python-benefits');
+    pythonBenefits.classList.toggle('expanded'); // Add or remove "expanded" class for styling
+}
